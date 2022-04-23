0de98066c9cae23de243fda2a3f2bebbc64b752e
Vipin Mandale <vmandale@gmail.com>
2022-04-23 16:24:27 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,30 @@
+terraform {
+  cloud {
+    organization = "vmandale"
+    workspaces {
+      name = "Example-Workspace"
+    }
+  }
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.27"
+    }
+  }
+}
+
+
+provider "aws" {
+  profile = "default"
+  region  = "us-west-2"
+}
+
+resource "aws_instance" "app_server" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "ExampleAppServerInstance"
+  }
+}
