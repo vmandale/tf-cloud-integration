6d6f12b51bd0b1ad777b0f53fd2b1b0a85a08f04
Vipin Mandale <vmandale@gmail.com>
2022-04-23 16:27:17 +0530

    Added aws_instance.app_server2
    Added aws_instance.app_server2 to create one more EC2 instance with the name ExampleAppServerInstance2

--- main.tf
+++ main.tf
@@ -28,3 +28,11 @@ resource "aws_instance" "app_server" {
     Name = "ExampleAppServerInstance"
   }
 }
+
+resource "aws_instance" "app_server2" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "ExampleAppServerInstance2"
+  }
+}
